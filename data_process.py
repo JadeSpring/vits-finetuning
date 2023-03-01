d7897427bd8d0f5151eaed5c607a195cec2bc11f
邵玉春 <yuchun.shao@ly.com>
2023-03-01 14:58:52 +0800

    feat:优化

--- data_process.py
+++ data_process.py
@@ -5,12 +5,12 @@ if __name__ == '__main__':
     xx = load_filepaths_and_text("filelists/wave_info.txt")
 
     # train_text = [f'{x[0]}|10|{x[1]}' for x in xx]
-    all_clean = [f'Wave/{x[0]}.wav|10|{x[2]}' for x in xx]
+    all_clean = [f'Wave/{x[0]}.wav|10|{x[1]}' for x in xx]
     train_clean = all_clean[:160]
     val_clean = all_clean[-32:]
 
-    train_file_path = 'filelist/train_text.cleaned'
-    val_file_path = 'filelist/val_text.cleaned'
+    train_file_path = 'filelist/train.text'
+    val_file_path = 'filelist/val.text'
 
     with open(train_file_path, "w", encoding="utf-8") as f:
         f.writelines([x + "\n" for x in train_clean])
